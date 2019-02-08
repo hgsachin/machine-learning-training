912043a8880181d95e1057ce63fcf48aec51617e
sharnahalli <sharnahalli@sapient.com>
2019-02-08 18:38:44 +0530

    Preprocessing and Simple Linear reg changes

---
+++ Part 1 - Data Preprocessing/data_prepro_template.py
@@ -0,0 +1,36 @@
+#Data Preprocessing
+#Importing libraries
+
+import numpy as np #Mathematical tools
+import matplotlib.pyplot as plt # to plot
+import pandas as pd # Import datasets & manage them
+
+# Importing dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+#Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X= oneHotEncoder.fit_transform(X).toarray()
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+#splitting dataset into the training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
